74d9304821d5ea0445c82f093eebda74008f504b
NishantSaurabh007 <145756982+NishantSaurabh007@users.noreply.github.com>
2023-09-22 09:46:16 +0000

    container config done

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,16 @@
+//  number, bigint, string ("within double qoute"), boolean (true/false) , null, undefined, symbol (uniqueness)
+
+const name = "Nishant";
+let age = 30;
+let city = "Barauni";
+let isLoggedIn = true;
+let temp = null;
+
+console.log(typeof name);
+console.log(typeof temp);
+console.log(typeof isLoggedIn);
+console.log(typeof age);
+
+
+
+
