13fda50682f5fed8a50c3c42687f0bcb21318423
NishantSaurabh007 <145756982+NishantSaurabh007@users.noreply.github.com>
2023-09-22 09:59:21 +0000

    data conversion

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,13 @@
+let score = "100";
+
+ console.log( score);
+let newValue = Number(score);
+console.log(newValue);
+
+console.table([typeof(score), typeof(newValue),]);
+
+let loggedStatus = "";
+
+let newStatus = Boolean(loggedStatus);
+
+console.log(typeof(newStatus));
