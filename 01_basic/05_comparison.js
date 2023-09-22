6f47fb1417094b3b9cf25c27f4a227bbb3601f24
NishantSaurabh007 <145756982+NishantSaurabh007@users.noreply.github.com>
2023-09-22 10:26:38 +0000

    Compaison done with same & different datatype

---
+++ 01_basic/05_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2!=1)
+
+console.log("2" > 1);
+console.log("01" < "02");
+console.log("01" > "02");
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 0);
+console.log(null == 0);
+
+console.log(undefined == 0);
+
+// === used for strict check
+// same datatype comparison should be done for clean code
+
+console.log("2" === 2);
