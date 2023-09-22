3c943f0ad489af204001b3747d437b50845e1608
NishantSaurabh007 <145756982+NishantSaurabh007@users.noreply.github.com>
2023-09-22 10:15:46 +0000

    Operations done

---
+++ 01_basic/04_operation.js
@@ -0,0 +1,34 @@
+let score1 = 200;
+let score2 = 300;
+
+let newScore = -score2;
+
+console.log(newScore);
+/*
+console.log(2+2) ---- addition
+console.log(2-2) ---- substraction
+console.log(2*2) ---- multiplication
+console.log(2/2) ---- division
+console.log(2%2) ---- module
+console.log(2**2) ---- power
+*/
+
+let stud1 = "Mohan"
+
+console.log("1" + 2);
+console.log(1 + "3" + 4);
+console.log(2 + 5 + "4" + 5);
+console.log("This is the score of me");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+++gameCounter;
+
+console.log(gameCounter);
+
+//sec-type-conversin 
+// prefix & postfix in MDN 
